1494941f686d1eb4c43a8d120fc19f4f10bdb2d3
Xipxap11 <davidplanaquerol@gmail.com>
2024-10-05 10:54:29 +0200

    Fin seccion 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  public gifsList: Gif[]=[];
   private _tagsHistory: string[] = []
   private apiKey: string = 'Wt8bOf9i0H35Ve3ip9Tbhwmsy6ABPbCp';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -25,6 +30,20 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if( !localStorage.getItem("history") ) return
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length===0)return;
+    this.searchTag(this.tagsHistory[0]);
+
   }
 
   searchTag(tag: string): void {
@@ -36,9 +55,9 @@ export class GifsService {
       .set("q", tag)
       .set("limit", '10')
 
-    this.http.get(`${this.serviceUrl}/search`, { params })
-      .subscribe(resp => {
-        console.log(resp);
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifsList=resp.data;
       })
     // const resp = await fetch('http://api.giphy.com/v1/gifs/search?api_key=Wt8bOf9i0H35Ve3ip9Tbhwmsy6ABPbCp&q=valorant&limit=10');
 
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -16,5 +16,8 @@ export class SidebarComponent {
     return this.gifsService.tagsHistory;
   }
 
+  searchTag(tag: string) {
+    this.gifsService.searchTag(tag);
+  }
 
 }
